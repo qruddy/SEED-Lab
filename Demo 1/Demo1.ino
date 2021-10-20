0c5bd6624659f2cde962ebf67c1513dbc9a92495
qruddy <63759188+qruddy@users.noreply.github.com>
2021-10-20 14:07:48 -0600

    Demo 1 Localization Arduino code

---
+++ Demo 1/Demo1.ino
@@ -0,0 +1,77 @@
+#include <Encoder.h>
+
+//declaration of wheels
+Encoder left(2,13);
+Encoder right(3,14);
+
+//pin declarations
+const int enablePin = 4;
+const int directionPinL = 7;
+const int directionPinR = 8;
+const int motorPinL = 9;
+const int motorPinR = 10;
+
+const double wheel_gap = 1; //distance between two wheels
+//d = 5.875 in
+//const double r = 2.9375; //radius of each wheel in inches
+const double r = 0.24479; //radius of each wheel in feet
+
+double final_position = 1;
+double final_angle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Encoder start: ");
+  pinMode(enablePin, HIGH);
+  pinMode(motorPinL, INPUT);
+  pinMode(motorPinR, INPUT);
+}
+
+long oldCountL = -999;
+long oldCountR = -999;
+double countL = 0;
+double countR = 0;
+double thetaL = 0;
+double thetaL_old = 0;
+double thetaR_old = 0;
+double thetaR = 0;
+double deltaThetaL = 0;
+double deltaThetaR = 0;
+double prevPosition = 0;
+double currentPosition = 0;
+
+void loop() {
+
+  analogWrite(motorPinL, 100);
+  analogWrite(motorPinR, 100);
+
+  //0 --> CW, 1--> CCW
+  digitalWrite(directionPinL, 0);
+  digitalWrite(directionPinR, 1);
+  
+  countL = left.read(); //read current number of counts for left wheel
+  countR = right.read(); //read current number of counts for right wheel
+  
+  if (countL != oldCountL) {
+    oldCountL = countL;
+    thetaL = 9 * (double) countL / 80; //current left wheel position in degrees
+    deltaThetaL = thetaL - thetaL_old;
+  }
+
+  if (countR != oldCountR) {
+    oldCountR = countR;
+    thetaR = 9 * (double) countR / 80; //current left wheel position in degrees
+    deltaThetaR = thetaL - thetaL_old;
+  }
+
+  currentPosition = prevPosition + deltaTheta * r;
+
+  if (currentPosition >= finalPosition) { //if final position is reached, turn the motors off
+    analogWrite(motorPinL, 0);
+    analogWrite(motorPinR, 0);
+  }
+
+  prevPosition = currentPosition;
+  thetaL_old = thetaL;
+  thetaR_old = thetaR;
+}
